cdc00615dfb608eee9642b456296786862a17965
KleversDS <klevers170@gmail.com>
2024-09-30 23:22:30 +0300

    Добавьте файлы проекта.

---
+++ Fp1/Fp1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{//1
+        cout << "Hopeless, a steadfast hope forever keeping,:\n\n";
+        cout << "I want to live! You thoughts of grief, away!:\n";
+        cout << "On poor sad fallow land unused to tilling,\n";
+        cout << "I'll sow blossoms, brilliant in hue,\n";
+        cout << "I'll sow blossoms where the frost lies, chilling.\n\n";
+        cout << "- Lesya Ukrainka\n" << endl;
+        //2
+            cout << "Every\n";
+            cout << "\tHunter\n";
+            cout << "\t\tWants\n";
+            cout << "\t\t\tTo\n";
+            cout << "\t\t\t\tKnow\n";
+            cout << "\t\t\t\t\tWhere\n";
+            cout << "\t\t\t\t\t\tThe\n";
+            cout << "\t\t\t\t\t\t\tPheasant\n";
+            cout << "\t\t\t\t\t\t\t\tSits.\n";
+            
+//3
+                cout << "I will sell a car.\n";
+                cout << "Brand: Toyota.\n";
+                cout << "Year of release: 2015.\n";
+                cout << "Run: 100000 км.\n";
+                cout << "Phone: 098-765-4321\n";
+                cout << "098 | 765 |\n";
+                cout << "432 | 1 |\n";
+
+}
